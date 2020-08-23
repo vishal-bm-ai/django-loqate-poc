5eb11bcd5bccc0eecea24b6a1c69dd59abfb5b4d
singhv1shal <singhvishal0304@gmail.com>
2020-08-23 17:27:09 +0530

    added front-end interaction

---
+++ address_lookup/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddressLookupConfig(AppConfig):
+    name = 'address_lookup'
---
+++ address_lookup/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomeView(TemplateView):
+	template_name = 'address_lookup/index.html'
